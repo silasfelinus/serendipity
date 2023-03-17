506560d66113bdead444fd6e9a8282c9e72661ea
silasfelinus <silasfelinus@gmail.com>
2023-03-17 13:01:41 -0700

    one-click launcher

--- applaunch.sh
+++
@@ -1,9 +0,0 @@
-python3.8 -m venv venv
-
-#Activate the virtual environment:
-source ./venv/Scripts/activate
-
-pip install -r requirements.txt
-source ./.env
-cd app
-gradio main.py
---
+++ launch.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Create the virtual environment if it doesn't exist
+if [ ! -d "venv" ]; then
+    python3.8 -m venv venv
+fi
+
+# Activate the virtual environment
+if [ "$(uname)" == "Darwin" ] || [ "$(uname)" == "Linux" ]; then
+    # macOS or Linux
+    source venv/bin/activate
+elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ] || [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+    # Windows (Git Bash)
+    source venv/Scripts/activate
+else
+    # Unsupported system
+    echo "Unsupported system. Please run the commands manually."
+    exit 1
+fi
+
+# Install the required packages
+#pip install -r requirements.txt
+
+# Source environment variables
+if [ -f .env ]; then
+    source ./.env
+else
+    echo "No .env file found, skipping sourcing environment variables."
+fi
+
+# Run the application
+python app/main.py
