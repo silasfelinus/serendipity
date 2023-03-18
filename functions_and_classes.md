5513e056ea9c9fff1e1f3ddca6daff5ddf7a8430
silasfelinus <silasfelinus@gmail.com>
2023-03-18 16:29:48 -0700

    updated trawler for project files scope

--- functions_and_classes.md
+++ functions_and_classes.md
@@ -5,6 +5,16 @@ app/
     logging_config.py
     main.py
     __init__.py
+    chatbot/
+        bot_config.py
+        chatbot.py
+        conversation_handler.py
+        messaging_manager.py
+        prompt_builder.py
+        response_handler.py
+        __init__.py
+        routes/
+            chatbot_routes.py
     interface/
         gradio.py
         __init__.py
@@ -13,41 +23,17 @@ app/
         livechat.py
         routes.py
         __init__.py
-    templates/
-        index.html
-        livechat.html
-    static/
-        livechat.js
-        styles.css
-    test/
-        test_main.py
-        __init__.py
-    chatbot/
-        chatbot.py
-        conversation_handler.py
-        bot_config.py
-        response_handler.py
-        messaging_manager.py
-        __init__.py
-        prompt_builder.py
-        routes/
-            chatbot_routes.py
     routes/
         routes.py
         __init__.py
+    test/
+        test_main.py
+        __init__.py
 ```
 
-# Serendipity Functions and Classes
+# Functions and Classes
 
-## 
-
-### Functions
-
-
-### Classes
-
-
-## /home/silasfelinus/code/serendipity/app/routes/routes.py
+## C:\Users\silas\code\serendipity\app\chatbot\messaging_manager.py
 
 ### Functions
 
@@ -55,7 +41,7 @@ app/
 ### Classes
 
 
-## app = Flask(__name__)
+## C:\Users\silas\code\serendipity\app\livechat\__init__.py
 
 ### Functions
 
@@ -63,7 +49,7 @@ app/
 ### Classes
 
 
-## from dotenv import load_dotenv
+## #./app/chatbot/bot_config.py
 
 ### Functions
 
@@ -71,7 +57,7 @@ app/
 ### Classes
 
 
-## /home/silasfelinus/code/serendipity/app/chatbot/prompt_builder.py
+## C:\Users\silas\code\serendipity\app\chatbot\prompt_builder.py
 
 ### Functions
 
@@ -79,7 +65,7 @@ app/
 ### Classes
 
 
-## /home/silasfelinus/code/serendipity/app/test/test_main.py
+## C:\Users\silas\code\serendipity\app\interface\gradio.py
 
 ### Functions
 
@@ -87,7 +73,200 @@ app/
 ### Classes
 
 
-## /home/silasfelinus/code/serendipity/app/chatbot/__init__.py
+## C:\Users\silas\code\serendipity\app\chatbot\chatbot.py
+
+### Functions
+
+
+### Classes
+
+
+## #./app/chatbot/messaging_manager.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\main.py
+
+### Functions
+
+
+### Classes
+
+
+## # app/routes/routes.py
+
+### Functions
+
+
+### Classes
+
+
+## #./app/chatbot/response_handler.py
+
+### Functions
+
+
+### Classes
+
+* `ResponseHandler`
+
+## #./app/chatbot/routes/chatbot_routes.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\interface\__init__.py
+
+### Functions
+
+
+### Classes
+
+
+## #./app/chatbot/chatbot.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\test\test_main.py
+
+### Functions
+
+
+### Classes
+
+
+## # app/test/test_main.py
+
+### Functions
+
+
+### Classes
+
+
+## #./app/chatbot/conversation_handler.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\chatbot\bot_config.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\__init__.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\livechat\livechat.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\test\__init__.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\livechat\events.py
+
+### Functions
+
+
+### Classes
+
+
+## #./app/main.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\chatbot\response_handler.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\livechat\routes.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\routes\routes.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\routes\__init__.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\chatbot\routes\chatbot_routes.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\logging_config.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\chatbot\conversation_handler.py
+
+### Functions
+
+
+### Classes
+
+
+## C:\Users\silas\code\serendipity\app\chatbot\__init__.py
 
 ### Functions
 
--- trawler.py
+++ trawler.py
@@ -1,27 +1,24 @@
 import os
 import fnmatch
+import re
 
-def is_ignored(path):
+
+def is_ignored(path, ignored_patterns):
     return any(fnmatch.fnmatch(path, pattern) for pattern in ignored_patterns)
 
-# Define the paths to the directories and files
-root_dir = os.getcwd()
-app_dir = os.path.join(root_dir, "app")
-gitignore_file = os.path.join(root_dir, ".gitignore")
-all_code_file = os.path.join(root_dir, "all_code.md")
-functions_file = os.path.join(root_dir, "functions_and_classes.md")
-directory_tree_file = os.path.join(root_dir, "directory_tree.txt")
 
-# Read the .gitignore file and extract patterns to ignore
-ignored_patterns = set()
-if os.path.isfile(gitignore_file):
-    with open(gitignore_file, "r") as f:
-        ignored_patterns = set(line.strip() for line in f if line.strip() and not line.startswith("#"))
+def get_ignored_patterns(gitignore_file):
+    ignored_patterns = {'.git','site-packages'}
+    if os.path.isfile(gitignore_file):
+        with open(gitignore_file, "r") as f:
+            ignored_patterns.update(line.strip() for line in f if line.strip() and not line.startswith("#"))
+    return ignored_patterns
 
-def traverse_directory(dir_path="."):
+
+def traverse_directory(dir_path, ignored_patterns):
     code_string = ""
     for root, dirs, files in os.walk(dir_path):
-        dirs[:] = [d for d in dirs if not is_ignored(d)]
+        dirs[:] = [d for d in dirs if not is_ignored(d, ignored_patterns)]
         for file_name in files:
             if file_name.endswith(".py"):
                 file_path = os.path.join(root, file_name)
@@ -30,19 +27,17 @@ def traverse_directory(dir_path="."):
                     code_string += f"{file_path}\n{code}\n-\n"
     return code_string
 
-def generate_directory_tree(start_path="."):
+
+def generate_directory_tree(start_path, ignored_patterns):
     tree_output = ""
     for root, dirs, files in os.walk(start_path):
-        # Check if the current directory should be ignored
-        if is_ignored(root):
+        if is_ignored(root, ignored_patterns):
             dirs[:] = []
             continue
 
-        # Exclude ignored directories from the search
-        dirs[:] = [d for d in dirs if not is_ignored(os.path.join(root, d))]
+        dirs[:] = [d for d in dirs if not is_ignored(os.path.join(root, d), ignored_patterns)]
 
-        # Only add files that are not ignored
-        relevant_files = [f for f in files if not is_ignored(os.path.join(root, f))]
+        relevant_files = [f for f in files if not is_ignored(os.path.join(root, f), ignored_patterns) and f.endswith(".py")] # Modified to include only .py files
         if relevant_files:
             level = root.replace(start_path, '').count(os.sep)
             indent = ' ' * 4 * (level)
@@ -52,47 +47,127 @@ def generate_directory_tree(start_path="."):
                 tree_output += f"{subindent}{file}\n"
     return tree_output
 
-with open(directory_tree_file, "w") as f:
-    f.write(generate_directory_tree())
 
-with open(all_code_file, "w") as f:
-    f.write(traverse_directory(app_dir))
+def generate_project_code(projects_file, ignored_patterns):
+    if not os.path.isfile(projects_file):
+        print(f"Error: {projects_file} not found.")
+        return
 
-def generate_functions_and_classes(input_file, output_file):
+    output_file = "project_code.txt"
+    with open(projects_file, "r") as f:
+        project_files = [line.strip() for line in f if line.strip()]
+
+    with open(output_file, "w") as f:
+        for file_path in project_files:
+            if not os.path.isfile(file_path):
+                print(f"Error: {file_path} not found in the specified projects.txt.")
+                continue
+            if not is_ignored(file_path, ignored_patterns):
+                with open(file_path, "r") as project_file:
+                    code = project_file.read()
+                    f.write(f"{file_path}\n{code}\n-\n")
+
+
+def create_projects_file(projects_file, root_dir, ignored_patterns):
+    with open(projects_file, "w") as f:
+        for root, dirs, files in os.walk(root_dir):
+            dirs[:] = [d for d in dirs if not is_ignored(os.path.join(root, d), ignored_patterns)]
+            for file_name in files:
+                file_path = os.path.join(root, file_name)
+                if not is_ignored(file_path, ignored_patterns) and file_name.endswith(".py"): # Modified to include only .py files
+                    f.write(f"{file_path}\n")
+
+
+def generate_functions_and_classes(input_file, output_file, app_dir, ignored_patterns):
     with open(input_file, "r") as f:
         code_string = f.read()
 
     lines = code_string.split("\n")
-    relevant_lines = []
-    relevant_files = set()
+    relevant_files = []
 
     for i, line in enumerate(lines):
-        if line.startswith("# "):
-            relevant_files.add(lines[i - 1])
-        elif (line.startswith("- def ") and "(" in line) or (line.startswith("- class ") and ":" in line):
-            relevant_lines.append(line)
+        if line.endswith(".py") and not any(ignored_pattern in line for ignored_pattern in ignored_patterns):
+            relevant_files.append(line.strip())
 
-    # Generate the directory tree
-    directory_tree = generate_directory_tree(app_dir)
+    directory_tree = generate_directory_tree(app_dir, ignored_patterns)
 
-    # Append the directory tree to the output string
-    output_string = f"# Directory Tree\n\n```\n{directory_tree}```\n\n# Serendipity Functions and Classes\n\n"
-    
+    output_string = ""
     for file_path in relevant_files:
-        output_string += f"## {file_path}\n\n"
-        output_string += f"### Functions\n\n"
-        for line in relevant_lines:
-            if line.startswith("- def ") and file_path in lines[i-1]:
-                function_name = line.split("def ")[1].split("(")[0]
-                output_string += f"* `{function_name}`\n"
-        output_string += f"\n### Classes\n\n"
-        for line in relevant_lines:
-            if line.startswith("- class ") and file_path in lines[i-1]:
-                class_name = line.split("class ")[1].split(":")[0]
-                output_string += f"* `{class_name}`\n"
-        output_string += "\n"
+        with open(file_path, "r") as f:
+            file_content = f.read()
+
+        # Extract imports
+        imports = []
+        for import_line in re.findall(r"^import\s.+", file_content, re.MULTILINE):
+            imports.append(import_line.strip())
+        for from_import_line in re.findall(r"^from\s.+import.+", file_content, re.MULTILINE):
+            imports.append(from_import_line.strip())
+
+        # Extract classes and functions
+        class_functions = []
+        for match in re.finditer(r"(class|def)\s+(\w+)\s*\(", file_content):
+            function_type = match.group(1)
+            function_name = match.group(2)
+            function_comment = ""
+
+            # Extract comment if available
+            comment_match = re.search(fr"{function_name}.*\n\s*\"\"\"(.*)\"\"\"", file_content)
+            if comment_match:
+                function_comment = comment_match.group(1)
+
+            class_functions.append((function_type, function_name, function_comment))
+
+        output_string += f"{file_path} "
+
+        # Add directory tree comment
+        for directory in directory_tree:
+            if directory in file_path:
+                output_string += f"#{directory}\n"
+
+        # Add imports
+        if imports:
+            output_string += "\n[imports] "
+            output_string += ", ".join(imports)
+            output_string += "\n"
+
+        # Add classes and functions
+        for function_type, function_name, function_comment in class_functions:
+            output_string += f"{function_name}("
+            function_params = re.search(f"{function_name}\((.*)\)", file_content).group(1).strip()
+            output_string += function_params
+            output_string += "):"
+            output_string += f" return #{function_comment}" if function_comment else "\n"
+            output_string += "\n"
 
     with open(output_file, "w") as f:
         f.write(output_string)
-        
-generate_functions_and_classes(all_code_file, functions_file)
+
+def main():
+    root_dir = os.getcwd()
+    app_dir = os.path.join(root_dir, "app")
+    gitignore_file = os.path.join(root_dir, ".gitignore")
+    all_code_file = os.path.join(root_dir, "all_code.md")
+    functions_file = os.path.join(root_dir, "functions_and_classes.md")
+    directory_tree_file = os.path.join(root_dir, "directory_tree.txt")
+    projects_file = os.path.join(root_dir, "projects.txt")
+
+    ignored_patterns = get_ignored_patterns(gitignore_file)
+
+    with open(directory_tree_file, "w") as f:
+        f.write(generate_directory_tree(root_dir, ignored_patterns))
+
+    with open(all_code_file, "w") as f:
+        f.write(traverse_directory(app_dir, ignored_patterns))
+
+    if os.path.isfile(projects_file):
+        generate_project_code(projects_file, ignored_patterns)
+    else:
+        create_projects_file(projects_file, root_dir, ignored_patterns)
+        generate_project_code(projects_file, ignored_patterns)
+
+    generate_functions_and_classes(all_code_file, functions_file, app_dir, ignored_patterns)
+
+
+if __name__ == "__main__":
+    main()
+
