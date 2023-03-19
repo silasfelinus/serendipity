84ae94b58b8ae14c326d6ed3e325ce61ecf78398
Silas M Knight <silasfelinus@gmail.com>
2023-03-19 00:51:16 -0700

    bug hunts

--- app/chatbot/chatbot.py
+++ app/chatbot/chatbot.py
@@ -1,3 +1,4 @@
+#./app/chatbot/chatbot.py
 from .bot_config import BotConfig
 from .conversation_handler import ConversationHandler
 from .messaging_manager import MessagingManager
@@ -9,11 +10,11 @@ class Chatbot:
         bot_config = BotConfig(config_file_path)
         self.conversation_handler = ConversationHandler(bot_config)
         self.messaging_manager = MessagingManager(bot_config)
-        self.prompt_builder = PromptBuilder(bot_config)
+        self.prompt_builder = PromptBuilder(bot_config, self.conversation_handler.conversation_history)
         self.response_handler = ResponseHandler(bot_config)
 
     def response(self, user_input, chatbot_id, conversation_history):
-        prompt = self.prompt_builder.build_prompt()
+        prompt = self.prompt_builder.build_prompt(user_input)
         processed_response = self.messaging_manager.generate_response(user_input, chatbot_id, conversation_history, prompt)
         self.conversation_handler.handle_conversation(user_input, chatbot_id, conversation_history)
         return processed_response
