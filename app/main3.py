11f722740d4d50f7c147ac546935511d4f953b69
silasfelinus <silasfelinus@gmail.com>
2023-03-23 16:47:11 -0700

    testingmethods, still no concurrent launch

--- app/main3.py
+++ app/main3.py
@@ -1,7 +1,8 @@
-import asyncio
+import threading
 import quart
 from gradio import Interface
 from quart import Quart, render_template
+import asyncio
 
 app = Quart(__name__)
 
@@ -13,25 +14,31 @@ async def home():
 def gradio_interface(x):
     return x[::-1]
 
+def run_gradio():
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
 
-async def run_gradio():
     iface = Interface(
-    fn=gradio_interface,
-    inputs="text",
-    outputs="text",
-    title="Gradio Interface",
-    description="Enter some text to reverse it!"
-)  # define your Gradio interface here
-    await iface.launch(server_name="0.0.0.0", port=5001, start_server=True)
+        fn=gradio_interface,
+        inputs="text",
+        outputs="text",
+        title="Gradio Interface",
+        description="Enter some text to reverse it!"
+    )
+    iface.launch(server_name="0.0.0.0", share=True, server_port=5001, disable_signal_handler=True)
 
-async def run_quart():
-    await app.run(host="0.0.0.0", port=5000)
+def run_quart():
+    app.run(host="0.0.0.0", port=5000)
 
-async def main():
-    gradio_task = asyncio.create_task(run_gradio())
-    quart_task = asyncio.create_task(run_quart())
+def main():
+    gradio_thread = threading.Thread(target=run_gradio)
+    quart_thread = threading.Thread(target=run_quart)
 
-    await asyncio.gather(gradio_task, quart_task)
+    gradio_thread.start()
+    quart_thread.start()
+
+    gradio_thread.join()
+    quart_thread.join()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
--- run.py
+++ run.py
@@ -4,4 +4,4 @@ import asyncio
 from app.main3 import main
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
