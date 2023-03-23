d1a5d91f3dab9a5fc00085bea264ea63e9a14dae
silasfelinus <silasfelinus@gmail.com>
2023-03-23 00:28:33 -0700

    trying quart over flask

--- app/main.py
+++ app/main.py
@@ -1,42 +1,45 @@
 # ./app/main.py
 
-from flask import Flask, render_template
+from quart import Quart, render_template
 from .logging_config import setup_logging
 from app.interface.gradio import predict, inputs, outputs
 import gradio as gr
-import threading
+from gradio.components import Textbox
+import asyncio
 
 # Set up logging
 logger = setup_logging()
 
-# Create Flask app
-app = Flask(__name__)
+# Create Quart app
+app = Quart(__name__)
 
 @app.route('/')
-def home():
+async def home():
     logger.info("Home route accessed.")
     return "Welcome to the application!"
 
 @app.route('/api')
-def api():
-    return render_template('api.html')
+async def api():
+    return await render_template('api.html')
 
-def start_flask_app():
-    app.run(host='0.0.0.0', port=5000)
-    
-def start_gradio_interface():
-    iface = gr.Interface(fn=predict, inputs=inputs, outputs=outputs, title="Wonderwidgets Unleashed")
-    iface.launch(share=True)
+async def start_gradio_interface():
+    iface = gr.Interface(fn=predict, inputs=inputs, outputs=outputs, title="My API Web Service")
+    await iface.run()
 
-def main():
+async def start_quart_app():
+    await app.run_task(host='0.0.0.0', port=5000)
+
+async def main():
     logger.info("Starting the application...")
-    flask_thread = threading.Thread(target=start_flask_app)
-    flask_thread.start()
 
-    gradio_thread = threading.Thread(target=start_gradio_interface)
-    gradio_thread.start()
-    app.run(host='0.0.0.0', port=5000)
-    logger.info("Finished executing the application.")
+    # Start Gradio interface
+    gradio_task = asyncio.create_task(start_gradio_interface())
+
+    # Start Quart app
+    quart_task = asyncio.create_task(start_quart_app())
+
+    # Wait for tasks to complete
+    await asyncio.gather(gradio_task, quart_task)
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
