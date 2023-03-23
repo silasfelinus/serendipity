8f08e76fca542feca5394726da894122df530e5f
silasfelinus <silasfelinus@gmail.com>
2023-03-22 23:29:28 -0700

    moved files

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 # ./app/main.py
 
-from flask import Flask
+from flask import Flask, render_template
 from .logging_config import setup_logging
+from interface.gradio import gradio as gr
 
 # Set up logging
 logger = setup_logging()
@@ -14,8 +15,13 @@ def home():
     logger.info("Home route accessed.")
     return "Welcome to the application!"
 
+@app.route('/api')
+def api():
+    return render_template('api.html')
+
 def main():
     logger.info("Starting the application...")
+    iface.launch(share=True, app_name="Webapp WonderWidgets", interface_name="API Web Service")
     app.run(host='0.0.0.0', port=5000)
     logger.info("Finished executing the application.")
 
